1ebfa3fb35c2a04374d866feb1523f6bf9ed8245
Jack Ponder <jackponder3@gmail.com>
2022-07-19 21:09:05 -0400

    added css grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+        <div className="bar">8</div>
+
+        <div>0</div>
+        <div>0</div>
+        <div>0</div>
+        <div>0</div>
+        <div>0</div>
+        <div>0</div>
+      </div>
     </div>
   );
 }
-
-export default App;
