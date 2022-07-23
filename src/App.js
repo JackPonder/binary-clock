ccbf9da49e873b2da55b5242f7431cb7bff06381
Jack Ponder <jackponder3@gmail.com>
2022-07-23 17:13:47 -0400

    updated cloock logic

--- src/App.js
+++ src/App.js
@@ -1,49 +1,41 @@
-import { useEffect } from "react";
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  useEffect(() => {
-    setInterval(() => {
-      const date = new Date();
-      const time = [
-        Math.floor(date.getHours() / 10 % 10),
-        Math.floor(date.getHours() % 10),
-        Math.floor(date.getMinutes() / 10 % 10),
-        Math.floor(date.getMinutes() % 10),
-        Math.floor(date.getSeconds() / 10 % 10),
-        Math.floor(date.getSeconds() % 10),
-      ];
+  setInterval(() => {
+    const date = new Date();
+    const time = [
+      Math.floor(date.getHours() / 10 % 10),
+      Math.floor(date.getHours() % 10),
+      Math.floor(date.getMinutes() / 10 % 10),
+      Math.floor(date.getMinutes() % 10),
+      Math.floor(date.getSeconds() / 10 % 10),
+      Math.floor(date.getSeconds() % 10),
+    ];
 
-      for (let i = 0; i < time.length; i++) {
-        document.getElementById("label" + i).children[0].innerHTML = time[i];
-      }
+    for (let i = 0; i < time.length; i++) {
+      const [ label1, label2 ] = document.getElementById("label" + i).children;
+      label1.innerHTML = time[i];
+      label2.innerHTML = time[i].toString(2).padStart(4, "0");
+    }
 
-      time.forEach((item, index) => { 
-        time[index] = item.toString(2).padStart(4, "0");
-      });
-
-      for (let i = 0; i < time.length; i++) {
-        document.getElementById("label" + i).children[1].innerHTML = time[i];
-      }
+    const binaryTime = time.map((item) => item.toString(2).padStart(4, "0")).join("");
+    for (let i = 0; i < binaryTime.length; i++) {
+      const label = 
+        (i - 3) % 4 === 0 ? 1 : 
+        (i - 2) % 4 === 0 ? 2 :
+        (i - 1) % 4 === 0 ? 4 : 8;
+      const bar = document.getElementById(i);
       
-      const binaryTime = time.join("");
-      for (let i = 0; i < binaryTime.length; i++) {
-        const label = 
-          (i - 3) % 4 === 0 ? 1 : 
-          (i - 2) % 4 === 0 ? 2 :
-          (i - 1) % 4 === 0 ? 4 : 8;
-        const bar = document.getElementById(i);
-        
-        if (binaryTime[i] === "1") {
-          bar.className = "bar active";
-          bar.innerHTML = label;
-        } else if (binaryTime[i] === "0") {
-          bar.className = "bar";
-          bar.innerHTML = "";
-        }
+      if (binaryTime[i] === "1") {
+        bar.className = "bar active";
+        bar.innerHTML = label;
+      } else if (binaryTime[i] === "0") {
+        bar.className = "bar";
+        bar.innerHTML = "";
       }
-    }, 1000);
-  }, []);
+    }
+  }, 1000);
+  
 
   return (
     <div className="App">
